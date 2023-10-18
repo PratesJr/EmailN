697b1a20d0f1835b41cc53e1d018cf4800150fbb
Marcelo Prates <mpratesjunior@gmail.com>
2023-10-18 20:34:05 -0300

    refactor: unit tests

--- internal/tests/service_test.go
+++ internal/tests/service_test.go
@@ -5,24 +5,24 @@ import (
 	"emailn/internal/domain/campaign"
 	"emailn/internal/exceptins"
 	"emailn/internal/mocks"
-	"errors"
+	"github.com/jaswdr/faker"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
-var (
-	newCampaign = contract.NewCampaign{
-		Name:    "Marcelo",
-		Content: "content",
-		Email:   []string{"teste@mail.com", "testeee@mail.com"},
-	}
-	service = campaign.Service{}
-)
-
 func TestService(t *testing.T) {
-	t.Run("should create a campaign swith no errors", func(t *testing.T) {
+	var (
+		fake        = faker.New()
+		newCampaign = contract.NewCampaign{
+			Name:    fake.Lorem().Text(13),
+			Content: "content",
+			Email:   []string{"teste@mail.com", "testeee@mail.com"},
+		}
+		service = campaign.Service{}
+	)
+	t.Run("should create a campaign with no errors", func(t *testing.T) {
 		assertions := assert.New(t)
 		repoMock := new(mocks.RepositoryMock)
 
@@ -68,13 +68,54 @@ func TestService(t *testing.T) {
 	t.Run("should return the correct db exception", func(t *testing.T) {
 		assertions := assert.New(t)
 		repoMock := new(mocks.RepositoryMock)
-
+		nCampaign := contract.NewCampaign{
+			Name:    fake.Lorem().Text(13),
+			Content: "content",
+			Email:   []string{"teste@mail.com", "testeee@mail.com"},
+		}
 		repoMock.On("Save", mock.Anything).Return(exceptins.DbError)
+
+		service.Repository = repoMock
+		_, err := service.Create(nCampaign)
+
+		assertions.NotNil(err)
+		assertions.Equal(exceptins.DbError.Error(), err.Error())
+	})
+	t.Run("should return the 'max' exception", func(t *testing.T) {
+		newCampaign.Name = fake.Lorem().Text(25)
+		assertions := assert.New(t)
+		repoMock := new(mocks.RepositoryMock)
+
+		repoMock.On("Save", mock.MatchedBy(func(c *campaign.Campaign) bool {
+			return c.Name == newCampaign.Name && c.Content == newCampaign.Content && len(c.Contacts) == len(newCampaign.Email)
+		})).Return(nil)
 
 		service.Repository = repoMock
 		_, err := service.Create(newCampaign)
 
 		assertions.NotNil(err)
-		assertions.True(errors.Is(exceptins.DbError, err))
+		assertions.Equal("Name is required with max 24", err.Error())
+	})
+	t.Run("should validate contacts required", func(t *testing.T) {
+		assertions := assert.New(t)
+
+		_, err := campaign.NewCampaign(
+			fake.Lorem().Text(13),
+			fake.Lorem().Text(100),
+			[]string{})
+
+		assertions.NotNil(err)
+		assertions.Equal("Contacts is required with min 1", err.Error())
+	})
+	t.Run("should validate contacts format", func(t *testing.T) {
+		assertions := assert.New(t)
+
+		_, err := campaign.NewCampaign(
+			fake.Lorem().Text(13),
+			fake.Lorem().Text(100),
+			[]string{"email"})
+
+		assertions.NotNil(err)
+		assertions.Equal("Email is not valid", err.Error())
 	})
 }
